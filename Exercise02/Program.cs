f361d713bb683407911589c7f4a6b409c74c040e
Oli <oli.varnavska@gmail.com>
2023-03-09 22:16:23 +0800

    Add Exercise 02

---
+++ Exercise02/Program.cs
@@ -0,0 +1,19 @@
+﻿int a = 44;
+int b = 8;
+
+if (a > b ) 
+{
+    Console.Write("max=");
+    Console.WriteLine(a);
+    Console.Write("min=");
+    Console.WriteLine(b);
+
+}
+else
+{
+     Console.Write("max=");
+    Console.WriteLine(b);
+    Console.Write("min=");
+    Console.WriteLine(a);
+}
+
