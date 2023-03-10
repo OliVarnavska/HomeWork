a90f33e916a4c35a23ecc1a5a4c19c911eb9ba64
Oli <oli.varnavska@gmail.com>
2023-03-10 21:16:57 +0800

    Add Exersice 08

---
+++ Exercise08/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int N = 2;
+
+if(number > 1)
+{
+    while(N <= number)
+    {
+        Console.Write(N + " ");
+        N = N + 2;
+    }
+}
